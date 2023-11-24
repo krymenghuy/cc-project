05dc56a62585ff04c6a27e113a0dc1a8b22ad811
krymenghuy <menghuykry@gmail.com>
2023-11-24 11:57:04 +0700

    web

--- java/main.js
+++ java/main.js
@@ -1,63 +1,5 @@
-// window.onscroll = function () {
-//   showBackToTopButton();
-//   stickyTop();
-// };
 
-// function showBackToTopButton() {
-//   var button = document.getElementById("backToTopBtn");
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     button.classList.add("show");
-//   } else {
-//     button.classList.remove("show");
-//   }
-// }
 
-// function stickyTop() {
-//   var header = document.querySelector(".top-page");
-//   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
-
-// function scrollToTop() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
-
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-// }
 let slideIndex = 0;
 showSlides();
 
@@ -78,4 +20,26 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active1";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+// button
+window.onscroll = function () {
+  showBackToTopButton();
+};
+
+function showBackToTopButton() {
+  var button = document.getElementById("backToTopBtn");
+  var header = document.querySelector(".top-page");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.classList.add("show");
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+    button.classList.remove("show");
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
