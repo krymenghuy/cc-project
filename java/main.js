649ed1820f849238a7ab6153f7a5e0266e444c25
krymenghuy <menghuykry@gmail.com>
2023-12-26 17:09:42 +0700

    my project

--- java/main.js
+++ java/main.js
@@ -1,5 +1,3 @@
-
-
 let slideIndex = 0;
 showSlides();
 
@@ -32,17 +30,16 @@ function showBackToTopButton() {
 
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     button.classList.add("show");
-    
   } else {
-    
     button.classList.remove("show");
   }
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
-    
   }
 }
 
@@ -50,3 +47,78 @@ function scrollToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+var searchIcon = document.querySelector(".open-search");
+var searchInput = document.querySelector(".search-open");
+
+var isOpen = false; // Flag to track the open/close state
+
+searchIcon.addEventListener("click", () => {
+  if (isOpen) {
+    // Close the search input
+    searchInput.style.width = "0";
+    searchInput.style.display = "none";
+    searchIcon.style.color = "#fff";
+  } else {
+    // Open the search input
+    searchInput.style.width = "210px";
+    searchInput.style.display = "flex";
+    searchIcon.style.color = "#000";
+  }
+
+  isOpen = !isOpen; // Toggle the open/close state
+});
+// btn closs
+document.addEventListener('DOMContentLoaded', function() {
+  const navButton = document.getElementById('navButton');
+  const stopButton = document.getElementById('stopButton');
+  const navMenu = document.getElementById('navMenu');
+  const menu_responsive = document.getElementById('menu_responsive');
+  
+  // Open navigation menu
+  navButton.addEventListener('click', function() {
+    navMenu.style.left = '0';
+    menu_responsive.style.display = 'block';
+    menu_responsive.style.left = '0';
+    navMenu.classList.remove('closed');
+    stopButton.style.display = 'block';
+    navButton.style.display = 'none';
+  });
+
+  // Close navigation menu
+  stopButton.addEventListener('click', function() {
+    navMenu.style.left = '-100%';
+    menu_responsive.style.display = 'none';
+    navMenu.classList.add('closed');
+    stopButton.style.display = 'none';
+    navButton.style.display = 'block';
+  });
+  // Close navigation menu
+  menu_responsive.addEventListener('click', function() {
+    navMenu.style.left = '-100%';
+    menu_responsive.style.display = 'none';
+    navMenu.classList.add('closed');
+    stopButton.style.display = 'none';
+    navButton.style.display = 'block';
+  });
+  
+})
+ /* script menu responsive */
+ toggleMenuResponsive();
+ function toggleMenuResponsive() {
+     $('body').on('click', '.menu-bar', function() {
+         $('.menu-responsive').toggleClass('menu-active');
+         $('.menu-responsive-cover').toggleClass('menu-active');
+         $("body").css("overflow", "hidden");
+     });
+     $('body').on('click', '#btnClose', function() {
+         $('.menu-responsive').toggleClass('menu-active');
+         $('.menu-responsive-cover').toggleClass('menu-active');
+         $("body").css("overflow", "auto");
+     });
+     $('body').on('click', '.menu-responsive-cover', function() {
+         $('.menu-responsive').toggleClass('menu-active');
+         $('.menu-responsive-cover').toggleClass('menu-active');
+         $("body").css("overflow", "auto");
+     });
+    }
